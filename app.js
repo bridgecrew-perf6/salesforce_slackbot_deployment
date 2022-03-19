94fc247495ad6d14b61384254c2dee855ea97b06
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2022-03-18 21:39:01 -0700

    init salesforce slackbot

---
+++ app.js
@@ -0,0 +1,14 @@
+const { App } = require('@slack/bolt');
+
+cont { App } = require('@slack/bolt');
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SIGNING_SECRET,
+    port: process.env.PORT || 3000
+});
+
+(async () => {
+    await app.start();
+    console.log("bolt works");
+})();
