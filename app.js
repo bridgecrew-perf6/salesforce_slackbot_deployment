c5393a5854db69e1472f48ec617b96f1f38f64fc
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2022-03-19 19:38:00 -0700

    listening to events and mentions

--- app.js
+++ app.js
@@ -5,61 +5,37 @@ const { App } = pkg;
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SIGNING_SECRET
+    appToken: process.env.SLACK_APP_TOKEN,
+    developerMode:true,
+    socketMode: true,
 });
 
-app.event('app_home_opened', async ({ event, client, context }) => {
+app.event('app_mention', async ({ event, context, client, say }) => {
     try {
-      /* view.publish is the method that your app uses to push a view to the Home tab */
-      const result = await client.views.publish({
-  
-        /* the user that opened your app's app home */
-        user_id: event.user,
-  
-        /* the view object that appears in the app home*/
-        view: {
-          type: 'home',
-          callback_id: 'home_view',
-  
-          /* body of the view */
-          blocks: [
-            {
-              "type": "section",
-              "text": {
-                "type": "mrkdwn",
-                "text": "*Welcome to your _App's Home_* :tada:"
-              }
+      await say({"blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Thanks for the mention <@${event.user}>! Here's a button`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Button",
+              "emoji": true
             },
-            {
-              "type": "divider"
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "mrkdwn",
-                "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-              }
-            },
-            {
-              "type": "actions",
-              "elements": [
-                {
-                  "type": "button",
-                  "text": {
-                    "type": "plain_text",
-                    "text": "Click me!"
-                  }
-                }
-              ]
-            }
-          ]
+            "value": "click_me_123",
+            "action_id": "first_button"
+          }
         }
-      });
+      ]});
     }
     catch (error) {
       console.error(error);
     }
-});
+  });
 
 
 (async () => {
